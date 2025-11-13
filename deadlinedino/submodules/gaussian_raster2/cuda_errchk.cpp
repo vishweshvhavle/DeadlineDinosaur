56ed6440837c2632a71760f2cc983e521512fe97
vishweshvhavle <vishweshvhavle@gmail.com>
2025-11-13 21:25:38 +0530

    Added FastLanczos

--- deadlinedino/submodules/gaussian_raster2/cuda_errchk.cpp
+++
@@ -1,9 +0,0 @@
-#include"cuda_errchk.h"
-#include<stdio.h>
-void cuda_error_check(const char* file, const char* function)
-{
-    cudaDeviceSynchronize();
-    cudaError_t err = cudaGetLastError();
-    if (err != cudaSuccess)
-        printf("Error in %s.%s : %s\n", file, function, cudaGetErrorString(err));
-}
